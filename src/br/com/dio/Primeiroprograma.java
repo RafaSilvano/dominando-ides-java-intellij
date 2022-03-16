4179c19d44eef0d1e4c50d913677dba28665b1a3
RafaSilvano <rafael.alves.silvano@gmail.com>
2022-03-16 20:33:25 -0300

    First commit

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Primeiroprograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+//        System.out.println("Hello World no IntelliJ");
+    }
+}
